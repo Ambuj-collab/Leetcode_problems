b040e17713d06c9715cb1ad57c7f4ee0dbe7ed9b
Ambuj Shukla <ashukla2@opentext.com>
2025-04-23 02:22:35 +0530

    fourth commit

---
+++ Array/118. Pascal's Triangle/Pascal's_Triangle_approach1.java
@@ -0,0 +1,130 @@
+/*
+
+Intuition:
+==============
+The problem of generating Pascal's triangle can be approached in various ways. Here are some approaches and their intuition to solve the problem:
+
+Approach 1: Using Recursion
+==============================
+Intuition: In Pascal's triangle, each element is the sum of the two elements directly above it. We can use a recursive approach to generate the triangle. The base case is when numRows is 1, in which case we return [[1]]. Otherwise, we recursively generate the triangle for numRows - 1 and then calculate the current row by summing the adjacent elements from the previous row.
+
+*/
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows == 0)
+            return new ArrayList<>();
+        if (numRows == 1) {
+            List<List<Integer>> result = new ArrayList<>();
+            result.add(Arrays.asList(1));
+            return result;
+        }
+
+        List<List<Integer>> prevRows = generate(numRows - 1);
+        List<Integer> newRow = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            newRow.add(1);
+        }
+
+        for (int i = 1; i < numRows - 1; i++) {
+            newRow.set(i, prevRows.get(numRows - 2).get(i - 1) + prevRows.get(numRows - 2).get(i));
+        }
+
+        prevRows.add(newRow);
+        return prevRows;
+    }
+}
+
+=============================================================================================================
+														OR
+=============================================================================================================
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) {
+            return result;
+        }
+
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        result.add(firstRow);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = result.get(i - 1);
+            List<Integer> currentRow = new ArrayList<>();
+            currentRow.add(1);
+
+            for (int j = 1; j < i; j++) {
+                currentRow.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+
+            currentRow.add(1);
+            result.add(currentRow);
+        }
+
+        return result;
+    }
+}
+
+=============================================================================================================
+														OR
+=============================================================================================================
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+
+            for (int j = 1; j < i; j++) {
+                int val = triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j);
+                row.add(val);
+            }
+
+            if (i > 0)
+                row.add(1);
+			
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+}
+
+=============================================================================================================
+														OR
+=============================================================================================================
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) {
+            return result;
+        }
+
+        if (numRows == 1) {
+            List<Integer> firstRow = new ArrayList<>();
+            firstRow.add(1);
+            result.add(firstRow);
+            return result;
+        }
+
+        result = generate(numRows - 1);
+        List<Integer> prevRow = result.get(numRows - 2);
+        List<Integer> currentRow = new ArrayList<>();
+        currentRow.add(1);
+
+        for (int i = 1; i < numRows - 1; i++) {
+            currentRow.add(prevRow.get(i - 1) + prevRow.get(i));
+        }
+
+        currentRow.add(1);
+        result.add(currentRow);
+
+        return result;
+    }
+}
